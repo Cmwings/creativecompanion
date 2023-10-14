f90623dcb52338ab8cf1a24b859db167652142ed
Cmwings <Phelpy1@outlook.com>
2023-10-13 19:05:43 -0700

    10/13 commit completed milestone 2

---
+++ app/src/main/java/com/example/creativecompanion/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.creativecompanion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // setting up buttons
+        ImageButton mediumButton = findViewById(R.id.Medium);
+        ImageButton colorButton = findViewById(R.id.color);
+        ImageButton ideaButton = findViewById(R.id.idea);
+        ImageButton paperButton = findViewById(R.id.Paper);
+        ImageButton drawingButton = findViewById(R.id.Drawing);
+        ImageButton canvasButton = findViewById(R.id.Canvas);
+        ImageButton toolsButton = findViewById(R.id.Tools);
+        ImageButton projectsButton = findViewById(R.id.Projects);
+        ImageButton moodButton = findViewById(R.id.Mood);
+        ImageButton favoriteButton = findViewById(R.id.favorite);
+        ImageButton todoButton = findViewById(R.id.todolist);
+
+        //on click listener to open new pages
+        mediumButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Medium.class)));
+        ideaButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Idea.class)));
+        colorButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ColorPalette.class)));
+        paperButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Paper.class)));
+        drawingButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Drawing.class)));
+        canvasButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Canvas.class)));
+        toolsButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Tools.class)));
+        projectsButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Projects.class)));
+        moodButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Mood.class)));
+        favoriteButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Favorite.class)));
+        todoButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ToDoList.class)));
+    }
+}
---
+++ app/src/main/java/com/example/creativecompanion/MyDatabaseHelper.java
@@ -0,0 +1,115 @@
+package com.example.creativecompanion;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_TABLE = "InventoryTable";
+
+    public static final String COL_2 = "Type";
+    public static final String COL_3 = "Brand";
+    public static final String COL_4 = "Description";
+    public static final String COL_5 = "Size";
+    public static final String COL_6 = "Qty";
+    public static final String COL_7 = "Website";
+
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+     //   SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + DATABASE_TABLE + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Type TEXT, Brand TEXT, Description TEXT, Size TEXT, Qty INTEGER, Website TEXT)");
+    }
+
+    public void addNewProduct(String type, String brand, String description, String size, Integer qty, String website) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+
+        // Passing values
+        values.put(COL_2, type);
+        values.put(COL_3, brand);
+        values.put(COL_4, description);
+        values.put(COL_5, size);
+        values.put(COL_6, qty);
+        values.put(COL_7, website);
+
+        // inserting data into database
+        db.insert(DATABASE_TABLE, null, values);
+
+        // closing database
+        db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Handle upgrade of database, this will delete all data
+        db.execSQL("DROP TABLE IF EXISTS DATABASE_TABLE;");
+        onCreate(db);
+    }
+
+
+    public ArrayList<String> getInventory(String searchTerm) {
+        ArrayList<String> list = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = {
+                MyDatabaseHelper.COL_2,
+                MyDatabaseHelper.COL_3,
+                MyDatabaseHelper.COL_4,
+                MyDatabaseHelper.COL_5,
+                MyDatabaseHelper.COL_6,
+                MyDatabaseHelper.COL_7
+        };
+        String selection = MyDatabaseHelper.COL_2 + " LIKE ?";
+        String[] selectionArgs = {"%" + searchTerm + "%"};
+
+        Cursor cursor = db.query(MyDatabaseHelper.DATABASE_TABLE, projection,
+                selection,
+                selectionArgs, null, null, null, null);
+
+        while (cursor.moveToNext()){
+            String column2Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_2));
+            String col2Text = "Type: ";
+            list.add(col2Text + column2Value);
+            String column3Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_3));
+            String col3Text = "Brand: ";
+            list.add(col3Text + column3Value);
+            String column4Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_4));
+            String col4Text = "Description: ";
+            list.add(col4Text + column4Value);
+            String column5Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_5));
+            String col5Text = "Size: ";
+            list.add(col5Text + column5Value);
+            String column6Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_6));
+            String col6Text = "QTY: ";
+            list.add(col6Text + column6Value);
+            String column7Value = cursor.getString(cursor.getColumnIndexOrThrow(MyDatabaseHelper.COL_7));
+            String col7Text = "Website: ";
+            list.add(col7Text + column7Value);
+
+        }
+        cursor.close();
+
+        return list;
+
+
+        }
+
+
+
+}
